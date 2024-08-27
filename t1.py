5ec7b6ed734ac7439f8a22a8d59b605c7309c24a
annant2005 <119773499+annant2005@users.noreply.github.com>
2024-08-27 10:37:39 +0530

    Up.py

--- t1.py
+++ t1.py
@@ -4,6 +4,7 @@ from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
 import pyttsx3
+import time
 
 
 cap = cv2.VideoCapture(0)
@@ -14,16 +15,19 @@ offset = 20
 imgSize = 300
 labels = ["hello", "correct", "no", "A", "B"]
 speak = ""
+prev_label = ""
+debounce_time = 1  # Minimum time between predictions in seconds
+last_prediction_time = time.time()  # Last time prediction was made
 
 while True:
-   
+    # Capture frame
     success, img = cap.read()
     if not success:
         break
     
     imgOutput = img.copy()
     
-   
+
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
@@ -33,7 +37,7 @@ while True:
         if imgCrop.size == 0:
             continue
         
-     
+      
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
         aspectRatio = h / w
         if aspectRatio > 1:
@@ -49,32 +53,37 @@ while True:
             hGap = math.ceil((imgSize - hCal) / 2)
             imgWhite[hGap:hCal + hGap, :] = imgResize
         
-     
-        prediction, index = classifier.getPrediction(imgWhite, draw=False)
-        label = labels[index]
-
-        if label != speak:
-            eng.say(label)
-            eng.runAndWait()
-            speak = label
+      
+        current_time = time.time()
+        if current_time - last_prediction_time > debounce_time:
         
-       
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            label = labels[index]
+            
+      
+            if label != prev_label:
+                eng.say(label)
+                eng.runAndWait()
+                prev_label = label
+            
+            last_prediction_time = current_time
+        
+  
         cv2.rectangle(imgOutput, (x - offset, y - offset - 50),
                       (x - offset + 90, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
-        cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        cv2.putText(imgOutput, prev_label, (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
         cv2.rectangle(imgOutput, (x - offset, y - offset),
                       (x + w + offset, y + h + offset), (255, 0, 255), 4)
         
-       
+
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
     
     cv2.imshow("Image", imgOutput)
     
-   
+
     if cv2.waitKey(1) == ord('q'):
         break
-
 
 cap.release()
 cv2.destroyAllWindows()
